0a40e10d26d42f1e202bf53b08a721c262c7273e
aygin <ayginnaderi@gmail.com>
2025-10-09 17:00:12 +0200

    Removed extra files

--- board.cmake
+++ board.cmake
@@ -1,6 +1,4 @@
-# SPDX-License-Identifier: Apache-2.0
-
-if(CONFIG_BOARD_EMBEVER_FEATHER_NRF9151_NRF9151_NS)
+if(CONFIG_BOARD_EMBEVER_FEATHER_NRF9151_NS)
   set(TFM_PUBLIC_KEY_FORMAT "full")
 endif()
 
@@ -8,11 +6,10 @@ if(CONFIG_TFM_FLASH_MERGED_BINARY)
   set_property(TARGET runners_yaml_props_target PROPERTY hex_file tfm_merged.hex)
 endif()
 
-# TODO: change to nRF9151_xxAA when such device is available in JLink
-board_runner_args(jlink "--device=nRF9160_xxAA" "--speed=4000")
-board_runner_args(pyocd "--target=nRF9160_xxAA" "--frequency=4000000")
+board_runner_args(jlink "--device=nRF9151_xxCA" "--speed=4000")
 board_runner_args(probe-rs "--chip=nRF9151_xxAA")
+
+
 include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
-include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/probe-rs.board.cmake)
--- sysbuild.cmake
+++
@@ -1,9 +0,0 @@
-#
-# Copyright (c) 2024 Circuit Dojo LLC
-# Copyright (c) 2025 Embever GmbH
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-set(PM_STATIC_YML_FILE ${CMAKE_CURRENT_LIST_DIR}/embever_feather_nrf9151_pm_static.yml CACHE INTERNAL "")
-
