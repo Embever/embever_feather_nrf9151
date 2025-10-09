76575beb9098acee49171845969394da41ed79ab
aygin <ayginnaderi@gmail.com>
2025-10-09 12:59:06 +0200

    Initial devicetree definition for feather nRF9151 v1.1

---
+++ sysbuild.cmake
@@ -0,0 +1,9 @@
+#
+# Copyright (c) 2024 Circuit Dojo LLC
+# Copyright (c) 2025 Embever GmbH
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set(PM_STATIC_YML_FILE ${CMAKE_CURRENT_LIST_DIR}/embever_feather_nrf9151_pm_static.yml CACHE INTERNAL "")
+
